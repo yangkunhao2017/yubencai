dfa4ca747ed3fbfb8615cb42e87df903d35a9804
yangkunhao <563161210@qq.com>
2022-05-16 15:02:47 +0800

    基本信息

--- pages/my-center/base-info.vue
+++ pages/my-center/base-info.vue
@@ -1,6 +1,43 @@
 <template>
 	<view>
-		
+		<view class="uni-flex uni-column view-vertical-center">
+      <image class="message-list-box-logo" src="../../../static/logo.png" ></image>
+      <text>点击修改头像</text>
+    </view>
+    <view style="background-color: #FFFFFF">
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>昵称</text>
+        <text>11111</text>
+      </view>
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>性别</text>
+        <text>11111</text>
+      </view>
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>生日</text>
+        <text>11111</text>
+      </view>
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>身高</text>
+        <text>11111</text>
+      </view>
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>学历</text>
+        <text>11111</text>
+      </view>
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>现居住地</text>
+        <text>11111</text>
+      </view>
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>手机号码</text>
+        <text>11111</text>
+      </view>
+      <view class="uni-flex uni-row view-vertical-center view-between list-item">
+        <text>邮箱</text>
+        <text>11111</text>
+      </view>
+    </view>
 	</view>
 </template>
 
@@ -18,5 +55,14 @@
 </script>
 
 <style>
-
+image{
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+}
+.list-item{
+  padding: 10px 0;
+  margin: 10px;
+  border-bottom: 1px solid #e5e5e5;
+}
 </style>
